6a7f6ff4e551c40627b07eadb7f63800ce628a09
Xuan Mai <xtm1109@westminstercollege.edu>
2016-04-17 18:43:28 -0600

    gameplay: asking permission for Fit API

--- app/src/main/java/com/example/xuan/pet2fit/MainActivity.java
+++ app/src/main/java/com/example/xuan/pet2fit/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.xuan.pet2fit;
 
 import android.app.Activity;
+import android.content.IntentSender;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.request.OnDataPointListener;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements OnDataPointListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+    private GoogleApiClient mApiClient = null;
+    private static final int REQUEST_OAUTH = 1;
+    private static final String AUTH_PENDING = "auth_state_pending";
+    private boolean authInProgress = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Fitness.SENSORS_API)
+                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if(!authInProgress) {
+            try {
+                authInProgress = true;
+                connectionResult.startResolutionForResult(MainActivity.this, REQUEST_OAUTH);
+            } catch(IntentSender.SendIntentException e) {}
+        } else {
+            System.out.println("onConnectionFailed: authInProgress");
+        }
+    }
+
+    @Override
+    public void onDataPoint(DataPoint dataPoint) {
+
     }
 }
